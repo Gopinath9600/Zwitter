083d8dfbc6578b4a7ad822f937cace3109544838
Gopinath Barat <baratgnath@gmail.com>
2024-02-02 22:21:44 +0530

    Displaying the hard-coded posts in the Posts componet using Post card

--- client/src/components/postSide/PostSide.jsx
+++ client/src/components/postSide/PostSide.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./PostSide.css";
 import PostShare from "../postShare/PostShare";
-// import Posts from '../posts/Posts'
+import Posts from "../posts/Posts";
 
 const PostSide = () => {
   return (
     <div className="PostSide">
       <PostShare />
-      {/* <Posts/> */}
+      <Posts />
     </div>
   );
 };
---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import "./Posts.css";
+import { PostsData } from "../../Data/PostsData.js";
+import Post from "../post/Post.jsx";
+// import { useDispatch, useSelector } from 'react-redux'
+// import { getTimelinePosts } from '../../actions/postAction.js'
+// import {useParams} from 'react-router-dom'
+
+const Posts = () => {
+  //   const dispatch = useDispatch();
+  //   const {user} = useSelector( (state)=> state.authReducer.authData)
+  //   let {posts,loading} = useSelector( (state)=> state.postReducer )
+  //   const params = useParams()
+
+  //   useEffect( ()=>{
+  //     dispatch (getTimelinePosts(user._id))
+  //   },[])
+
+  //   if(!posts) return "No Posts!"
+  //   if(params.id) posts = posts.filter((post)=> post.userId === params.id)
+  return (
+    <div className="Posts">
+      {PostsData.map((post, id) => {
+        return <Post data={post} />;
+      })}
+
+      {/* {loading?"Fetching Posts...":
+          posts.map( (post,id)=>{
+            return <Post data={post} id={id} />
+          })
+        } */}
+    </div>
+  );
+};
+
+export default Posts;
