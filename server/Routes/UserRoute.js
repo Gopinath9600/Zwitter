7047b4e8849fd2b873a2f25e4bf015c8a377148f
Gopinath Barat <baratgnath@gmail.com>
2024-02-05 10:54:29 +0530

    same

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -7,14 +7,15 @@ import {
   getUser,
   updateUser,
 } from "../Controllers/UserController.js";
+import authMiddleWare from "../Middlewares/authMiddleware.js";
 
 const router = express.Router(); //creating an instance of express Router
 
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.put("/:id/follow", followUser);
-router.put("/:id/unfollow", UnFollowUser);
+router.put("/:id", authMiddleWare, updateUser);
+router.delete("/:id", authMiddleWare, deleteUser);
+router.put("/:id/follow", authMiddleWare, followUser);
+router.put("/:id/unfollow", authMiddleWare, UnFollowUser);
 
 export default router;
